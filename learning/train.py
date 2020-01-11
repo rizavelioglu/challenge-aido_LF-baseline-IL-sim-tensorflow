46f672b7c5e655a43c7b18a616a809d2194dd763
rizavelioglu <40141130+rizavelioglu@users.noreply.github.com>
2020-01-11 19:07:29 +0100

    Update train.py

--- learning/train.py
+++ learning/train.py
@@ -1,55 +1,129 @@
-import numpy as np
-from tqdm import tqdm
+# -*- coding: utf-8 -*-
 
+### Imports
+
+import numpy as np
+from keras.applications.vgg16 import VGG16
+from keras.optimizers import Adam, SGD
+from keras.losses import mean_squared_error as MSE
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from keras.layers import GlobalAveragePooling2D, Dense
+from keras.models import Model
+
+import pickle
 from _loggers import Reader
-from model import TensorflowModel
+
+"""### Plot Losses"""
+
+# Function to plot model's validation loss and validation accuracy
+def plot_model_history(model_history):
+    fig, axs = plt.subplots(1,2,figsize=(25,8))
+    # summarize history for accuracy
+    axs[0].plot(range(1,len(model_history.history['acc'])+1),model_history.history['acc'])
+    axs[0].plot(range(1,len(model_history.history['val_acc'])+1),model_history.history['val_acc'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1,len(model_history.history['acc'])+1),len(model_history.history['acc'])/10)
+    axs[0].legend(['train', 'val'], loc='best')
+    # summarize history for loss
+    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].legend(['train', 'val'], loc='best')
+    plt.savefig(STORAGE_LOCATION+'/VGG16#8_model_history.png')
+    
+    plt.show()
+
+"""### Config"""
 
 # configuration zone
 BATCH_SIZE = 32
-EPOCHS = 10
+EPOCHS = 50
 # here we assume the observations have been resized to 60x80
 OBSERVATIONS_SHAPE = (None, 60, 80, 3)
 ACTIONS_SHAPE = (None, 2)
 SEED = 1234
-STORAGE_LOCATION = "trained_models/behavioral_cloning"
+# TODO: change thepaths
+STORAGE_LOCATION = "the path of the model that will be saved  ->  desktop/trained_models/"
+DATA_LOCATION = "the path of training data   ->   desktop/data/"
 
-reader = Reader('train.log')
+"""### Load Data"""
+
+reader = Reader(DATA_LOCATION+'train-v10.log')
 
 observations, actions = reader.read()
 actions = np.array(actions)
 observations = np.array(observations)
 
-model = TensorflowModel(
-    observation_shape=OBSERVATIONS_SHAPE,  # from the logs we've got
-    action_shape=ACTIONS_SHAPE,  # same
-    graph_location=STORAGE_LOCATION,  # where do we want to store our trained models
-    seed=SEED  # to seed all random operations in the model (e.g., dropout)
-)
+x_train, x_validate, y_train, y_validate = train_test_split(observations, actions, test_size = 0.2, random_state = 2)
 
-# we trained for EPOCHS epochs
-epochs_bar = tqdm(range(EPOCHS))
-for i in epochs_bar:
-    # we defined the batch size, this can be adjusted according to your computing resources...
-    loss = None
-    for batch in range(0, len(observations), BATCH_SIZE):
-        loss = model.train(
-            observations=observations[batch:batch + BATCH_SIZE],
-            actions=actions[batch:batch + BATCH_SIZE]
-        )
+train_datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0, # Randomly zoom image 
+        width_shift_range=0,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
 
-    epochs_bar.set_postfix({'loss': loss})
+train_datagen.fit(x_train)
 
-    # every 10 epochs, we store the model we have
-    # but I'm sure that you're smarter than that, what if this model is worse than the one we had before
-    if i % 10 == 0:
-        model.commit()
-        epochs_bar.set_description('Model saved...')
-    else:
-        epochs_bar.set_description('')
+validation_datagen = ImageDataGenerator()
+validation_datagen.fit(x_validate)
 
-# the loss at this point should be on the order of 2e-2, which is far for great, right?
+"""### Model"""
 
-# we release the resources...
-model.close()
-reader.close()
+base_model = VGG16(classes=2, input_shape=(60,80,3), weights=None, include_top=False)
 
+# add a global spatial average pooling layer
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+# let's add a fully-connected layer
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(2)(x)
+
+# this is the model we will train
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Define the optimizer
+optimizer = SGD(lr=0.01, momentum=0.001, nesterov=False)
+# optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+# Compile the model
+model.compile(optimizer = optimizer ,
+              loss = MSE, 
+              metrics=["accuracy"])
+
+es = EarlyStopping(monitor='val_loss', verbose=1, patience=30)
+mc = ModelCheckpoint(STORAGE_LOCATION+'VGG16#8.h5', monitor='val_loss', save_best_only=True)
+
+history = model.fit_generator(train_datagen.flow(x_train,y_train, batch_size=BATCH_SIZE),
+                              validation_data = validation_datagen.flow(x_validate,y_validate, batch_size=BATCH_SIZE),
+                              epochs = EPOCHS,
+                              verbose=2,  # for hiding print statements
+                              steps_per_epoch=observations.shape[0] // BATCH_SIZE,
+                              callbacks=[es, mc],
+                              shuffle=True)
+plot_model_history(history)
+
+"""### Evaluate Model"""
+
+from keras.models import load_model
+model = load_model(STORAGE_LOCATION + 'VGG16#8.h5')
+model.evaluate(x_validate,y_validate)
+
+num = 20
+preds = model.predict(x_validate[:num])
+
+for i in range(num):
+  print("Pred: ", preds[i], "\tGT: ", y_validate[i])
