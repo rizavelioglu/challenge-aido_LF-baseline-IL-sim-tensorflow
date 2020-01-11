896944460a5a0db2335670e21e080fc95d3a3fa8
rizavelioglu <40141130+rizavelioglu@users.noreply.github.com>
2020-01-11 18:57:32 +0100

    Update env.py

--- learning/env.py
+++ learning/env.py
@@ -1,22 +1,18 @@
-import gym
-import gym_duckietown
+from gym_duckietown.simulator import Simulator
 
-def launch_env(id=None):
-    env = None
-    if id is None:
-        from gym_duckietown.simulator import Simulator
-        env = Simulator(
-            seed=123, # random seed
-            map_name="loop_empty",
-            max_steps=500001, # we don't want the gym to reset itself
-            domain_rand=0,
-            camera_width=640,
-            camera_height=480,
-            accept_start_angle_deg=4, # start close to straight
-            full_transparency=True,
-            distortion=True,
-        )
-    else:
-        env = gym.make(id)
+
+def launch_env():
+    env = Simulator(
+        seed=123,  # random seed
+        map_name="zigzag_dists",
+        max_steps=500001,  # we don't want the gym to reset itself
+        domain_rand=True,
+        camera_width=640,
+        camera_height=480,
+        accept_start_angle_deg=1,  #4 start close to straight
+        full_transparency=True,
+        distortion=True,
+        randomize_maps_on_reset=True,
+    )
 
     return env
